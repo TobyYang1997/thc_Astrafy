8119c09f873535e883dbd45ec51392c8c45a7fc2
TobyYang1997 <56285707+TobyYang1997@users.noreply.github.com>
2022-08-23 19:00:31 +0200

    additional comments

--- thc_coding_question_1.sql
+++ thc_coding_question_1.sql
@@ -1,4 +1,6 @@
-SELECT *
-FROM `bigquery-public-data.crypto_bitcoin_cash.transactions`
-WHERE DATE(block_timestamp) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
+SELECT * --select all columns 
+FROM `bigquery-public-data.crypto_bitcoin_cash.transactions` --set dataset source
+WHERE DATE(block_timestamp) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE() 
+--set 3 months interval between the current time at the moment and the past
 ORDER BY block_timestamp ASC
+--sort by timestamp from the past
