90a37e2169c3101651ca65d65699093d12390d01
TobyYang1997 <56285707+TobyYang1997@users.noreply.github.com>
2022-08-23 19:33:31 +0200

    Update thc_coding_question_2.sql

--- thc_coding_question_2.sql
+++ thc_coding_question_2.sql
@@ -1,17 +1,17 @@
 WITH double_entry_book AS ( --pre-select data from input&outputs for later calculating balance later   
 SELECT
-array_to_string(inputs.addresses, ",") as address --transaction address, convert array to string for grouping
+array_to_string(inputs.addresses, ",") AS address --transaction address, convert array to string for grouping
 , inputs.type --types of transaction addresses
 , -inputs.value AS value --set inputs value as negative to subtract from outputs value
 , is_coinbase --column for identifying coinbase transactions
-FROM `bigquery-public-data.crypto_bitcoin_cash.transactions`,UNNEST(inputs) AS inputs --unpack nested field inputs 
+FROM `bigquery-public-data.crypto_bitcoin_cash.transactions`, UNNEST(inputs) AS inputs --unpack nested field inputs 
 UNION ALL --combine the results from the 2 select statements
 SELECT
 array_to_string(outputs.addresses, ",") AS address --transaction address, convert array to string for grouping
 , outputs.type --types of transaction addresses
 , outputs.value AS value --outputs transaction value
 , is_coinbase --column for identifying coinbase transactions
-FROM `bigquery-public-data.crypto_bitcoin_cash.transactions`,UNNEST(outputs) AS outputs --unpack nested field outputs 
+FROM `bigquery-public-data.crypto_bitcoin_cash.transactions`, UNNEST(outputs) AS outputs --unpack nested field outputs 
 )
 SELECT
 address --predefined above
